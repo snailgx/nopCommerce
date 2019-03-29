58812be815decc3c7636c7eff45e8ba3fdaba26d
skoshelev <serega.dn@gmail.com>
2019-03-29 16:29:46 +0300

    Issue 3451. Use redise to store plugins info data

---
+++ src/Libraries/Nop.Services/Plugins/RedisPluginsInfo.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Nop.Core.Infrastructure;
+using Nop.Core.Redis;
+using StackExchange.Redis;
+
+namespace Nop.Services.Plugins
+{
+    /// <summary>
+    /// Represents an information about plugins
+    /// </summary>
+    public partial class RedisPluginsInfo : PluginsInfo
+    {
+        #region Fields
+
+        private readonly IDatabase _db;
+
+        #endregion
+
+        #region Ctor
+
+        public RedisPluginsInfo(INopFileProvider fileProvider, IRedisConnectionWrapper connectionWrapper)
+            : base(fileProvider)
+        {
+            _db = connectionWrapper.GetDatabase(RedisDatabaseNumber.Plugin);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Save plugins info to the redis
+        /// </summary>
+        public override void Save()
+        {
+            var text = JsonConvert.SerializeObject(this, Formatting.Indented);
+            _db.StringSet(nameof(RedisPluginsInfo), text);
+        }
+
+        /// <summary>
+        /// Get plugins info
+        /// </summary>
+        /// <returns>True if data are loaded, otherwise False</returns>
+        public override bool LoadPluginInfo()
+        {
+            //try to get plugin info from the JSON file
+            var serializedItem = _db.StringGet(nameof(RedisPluginsInfo));
+
+            if (serializedItem.HasValue) 
+                return DeserializePluginInfo(serializedItem);
+
+            var loaded = false;
+
+            if (base.LoadPluginInfo())
+            {
+                Save();
+                loaded = true;
+            }
+
+            //delete the plugins info file
+            var filePath = _fileProvider.MapPath(NopPluginDefaults.PluginsInfoFilePath);
+            _fileProvider.DeleteFile(filePath);
+            
+            return loaded;
+        }
+
+        #endregion
+    }
+}
